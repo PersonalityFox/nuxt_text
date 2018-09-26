1a3ea111528854bf525340bddeb9e746a1185e34
Pavel <chiruxinp@gmail.com>
2018-09-26 23:41:52 +0300

    lesson 26 end

--- nuxt-2/pages/index.vue
+++ nuxt-2/pages/index.vue
@@ -1,66 +1,62 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        nuxt-2
-      </h1>
-      <h2 class="subtitle">
-        My well-made Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="home-page">
+      <section class="intro">
+        <h1>Get the latest tech news!</h1>
+      </section>
+      <section class="featured-posts">
+        <PostPreview 
+        id="1"
+        thumbnail="https://via.placeholder.com/950x150"
+        title="Hello there!"
+        previewText="This my first post!"/>
+        <PostPreview 
+        id="2"
+        thumbnail="https://via.placeholder.com/950x150"
+        title="Hello there! 2"
+        previewText="This my first post! 2"/>
+         <PostPreview 
+        id="3"
+        thumbnail="https://via.placeholder.com/950x150"
+        title="Hello there! 3"
+        previewText="This my first post! 3"/>
+      </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+import PostPreview from '@/components/Posts/PostPreview';
+export default{
+  components:{
+    PostPreview
   }
 }
 </script>
 
-<style>
+<style scroped>
+.post-preview{
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  width:90%;
+  background-color: white;
+}
+.home-page{
 
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.intro{
+
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+a{
+  text-decoration: none;
+  color:black;
 }
-.links
-{
-  padding-top: 15px;
+.post-thumbnail{
+  width:100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content{
+  padding: 10px;
+  text-align:center
 }
 </style>
