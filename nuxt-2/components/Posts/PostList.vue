94cb574c9b98c55a1e3b3049df00f71d96e13081
Personality_fox <chiruxinp@gmail.com>
2018-09-27 21:50:23 +0300

    lesson 30 end

---
+++ nuxt-2/components/Posts/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <section class="post-list">
+            <PostPreview 
+                id="1"
+                thumbnail="https://via.placeholder.com/950x150"
+                title="Hello there!"
+                previewText="This my first post!"/>
+            <PostPreview 
+                id="2"
+                thumbnail="https://via.placeholder.com/950x150"
+                title="Hello there! 2" 
+                previewText="This my first post! 2"/>
+            <PostPreview 
+                id="3"
+                thumbnail="https://via.placeholder.com/950x150"
+                title="Hello there! 3"
+                previewText="This my first post! 3"/>
+      </section>
+    </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+export default{
+  components:{
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- nuxt-2/pages/index.vue
+++ nuxt-2/pages/index.vue
@@ -3,69 +3,92 @@
       <section class="intro">
         <h1>Get the latest tech news!</h1>
       </section>
-      <section class="featured-posts">
-        <PostPreview 
-        id="1"
-        thumbnail="https://via.placeholder.com/950x150"
-        title="Hello there!"
-        previewText="This my first post!"/>
-        <PostPreview 
-        id="2"
-        thumbnail="https://via.placeholder.com/950x150"
-        title="Hello there! 2" 
-        previewText="This my first post! 2"/>
-         <PostPreview 
-        id="3"
-        thumbnail="https://via.placeholder.com/950x150"
-        title="Hello there! 3"
-        previewText="This my first post! 3"/>
-      </section>
+      <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview';
+import PostList from '@/components/Posts/PostList';
 export default{
   components:{
-    PostPreview
+    PostList
   }
 }
 </script>
 
 <style scroped>
-.post-preview{
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  width:90%;
-  background-color: white;
-}
-.intro{
+.intro {
   height: 300px;
   position: relative;
-  padding:30px;
+  padding: 30px;
   box-sizing: border-box;
   background-position: center;
   background-size: cover;
-  background-image: url('~assets/images/main-page-back.jpg');
 }
-.intro h1{
+
+.intro h1 {
   position: absolute;
-  top:10%;
-  left:5%;
-  width:90%;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
 }
-a{
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
   text-decoration: none;
-  color:black;
+  color: black;
 }
-.post-thumbnail{
-  width:100%;
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
   height: 200px;
   background-position: center;
   background-size: cover;
 }
-.post-content{
+
+.post-content {
   padding: 10px;
-  text-align:center
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
 }
 </style>
